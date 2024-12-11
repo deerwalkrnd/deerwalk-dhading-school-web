135b86ab197699cd62b4e218dc1eacf30bab8168
Supritphuyal <supritphuyal123@gmail.com>
2024-12-11 18:11:19 +0545

    Made changes to Academic Page and footer

--- app/_components/Footer/FooterCardIII.tsx
+++ app/_components/Footer/FooterCardIII.tsx
@@ -4,7 +4,7 @@ export default function FooterCardsIII() : ReactElement {
   return (
     <div className="flex flex-col items-center w-full h-full text-sm lg:text-lg text-start mx-auto text-white">
       <p>
-      Founded under the visionary guidance of Deerwalk Education Group, Sifal School is committed to delivering an exceptional education that aligns with the demands of the modern world. Our curriculum is thoughtfully designed to bridge classroom learning with real-world applications, ensuring our students are well-prepared for future challenges.
+      Founded under the visionary guidance of Deerwalk Education Group, Dhading School is committed to delivering an exceptional education that aligns with the demands of the modern world. Our curriculum is thoughtfully designed to bridge classroom learning with real-world applications, ensuring our students are well-prepared for future challenges.
       </p>
     </div>
   );
