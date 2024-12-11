daf2818cbfc36187d429bab67aee2347da7046a1
Supritphuyal <supritphuyal123@gmail.com>
2024-12-11 12:57:49 +0545

    Changed logo and managament photos

--- app/_sections/ClubsSection.tsx
+++ app/_sections/ClubsSection.tsx
@@ -1,27 +1,12 @@
 import React from "react";
 import Heading1 from "@/_components/Clubs/Heading1";
 import ClubsCards from "@/_components/Clubs/ClubsCards";
-import PoetryImage from "@/_assets/images/poetry.jpg";
 import DanceClubImage from "@/_assets/images/dance.jpg";
-import DebateImage from "@/_assets/images/debate.jpg";
 import yogaImage from "@/_assets/images/Yog.jpg";
 import MusicImage from "@/_assets/images/Music.jpg";
-import StemImage from "@/_assets/images/Stem.jpg";
-import ReadingImage from "@/_assets/images/reading.jpg";
-import NepaliReadingImage from "@/_assets/images/Nepalireading.jpg";
-import MathematicsImage from "@/_assets/images/Mathematics.jpg";
-import ForeignImage from "@/_assets/images/Foreign.jpg";
-import WritersImage from "@/_assets/images/writers.jpg";
-import NewsAndMediaImage from "@/_assets/images/News.jpg";
 import TableTennisImage from "@/_assets/images/tabletennis.jpg";
 import BadmintonImage from "@/_assets/images/badminton.jpg";
-import TaekwandoImage from "@/_assets/images/taekwando.jpg";
-import BasketballImage from "@/_assets/images/Basketball.jpg";
-import OlympiadImage from "@/_assets/images/Mathsolympiad.jpg";
 import ChessImage from "@/_assets/images/Chess.jpg";
-import FutsalImage from "@/_assets/images/Futsal.jpg";
-import TheatreImage from "@/_assets/images/Theater.jpg";
-import VisualArtsImage from "@/_assets/images/Arts.jpg";
 
 export default function ClubsSection() {
   return (
